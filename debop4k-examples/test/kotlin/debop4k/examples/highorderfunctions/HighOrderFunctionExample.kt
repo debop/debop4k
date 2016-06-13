3aa08756be3e731784ba036e58c4743beb7b2fd4
debop <sunghyouk.bae@gmail.com>
2016-06-13 18:49:23 +0900

    -. Lambda 를 이용하는 디자인 패턴 관련 추가.

--- debop4k-examples/test/kotlin/debop4k/examples/highorderfunctions/HighOrderFunctionExample.kt
+++ debop4k-examples/test/kotlin/debop4k/examples/highorderfunctions/HighOrderFunctionExample.kt
@@ -1,6 +1,11 @@
 package debop4k.examples.highorderfunctions
 
+import io.kotlintest.matchers.be
 import io.kotlintest.specs.FunSpec
+import java.io.BufferedReader
+import java.io.FileReader
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
 
 /**
  * @author sunghyouk.bae@gmail.com
@@ -21,11 +26,9 @@ class HighOrderFunctionExample : FunSpec() {
         this.forEach { item ->
           println("item=$item")
           if (predicate(item)) {
-            println("plus item=$item")
-            items += item
+            items.add(item)
           }
         }
-        println(items)
         return items
       }
 
@@ -35,5 +38,59 @@ class HighOrderFunctionExample : FunSpec() {
       list shouldBe listOf<Int>(4)
     }
 
+    test("Returning Function") {
+      val contacts = listOf(Person("Sunghyouk", "Bae", "123-4567"),
+                            Person("Misook", "Kwon", null))
+
+      with(ContactListFilters) {
+        prefix = "B"
+        onlyWithPhoneNumber = true
+      }
+      contacts.filter(ContactListFilters.getPredicate()) shouldBe listOf(Person("Sunghyouk", "Bae", "123-4567"))
+    }
+
+    test("inline functions : removing the overhead of lambda") {
+
+      val lock = ReentrantLock()
+      synchronized(lock) {
+        println("synchronized")
+      }
+
+    }
+
+    test("Lambda : Design Pattern for Resources") {
+      val lines = BufferedReader(FileReader("../README.md")).use { it.readLines() }
+      lines should be != null
+      lines.size should be gt 0
+      println(lines)
+    }
+
+  }
+}
+
+data class Person(val firstName: String, val lastName: String, val phoneNumber: String?)
+
+object ContactListFilters {
+  var prefix: String = ""
+  var onlyWithPhoneNumber: Boolean = false
+
+  fun getPredicate(): (Person) -> Boolean {
+    val startsWithPrefix = { p: Person ->
+      p.firstName.startsWith(prefix) || p.lastName.startsWith(prefix)
+    }
+    if (!onlyWithPhoneNumber) {
+      return startsWithPrefix
+    }
+    return { startsWithPrefix(it) && it.phoneNumber != null }
+  }
+}
+
+// inline 은 lambda 식을 매번 생성하지 않고, 컴파일 해서 쓴다. 매번 생성할 때보다 성능이 좋다.
+inline fun <T> synchronized(lock: Lock, action: () -> T): T {
+  lock.lock()
+  try {
+    return action()
+  } finally {
+    lock.unlock()
   }
 }
