99d2cc83ba74ec9335d4d779746610ecb424fae3
debop <sunghyouk.bae@gmail.com>
2016-06-23 17:09:52 +0900

    -. toCompletable Extension Function 추가.

--- debop4k-reactive/src/kotlin/debop4k/reactive/completable.kt
+++ debop4k-reactive/src/kotlin/debop4k/reactive/completable.kt
@@ -6,10 +6,16 @@ import rx.Single
 import rx.functions.Action0
 import java.util.concurrent.Callable
 import java.util.concurrent.Future
+import kotlin.reflect.KCallable
 
 fun <T> completableOf(f: () -> T): Completable = Completable.fromAction { f.invoke() }
+fun <T> completableOf(f: KCallable<T>): Completable = Completable.fromCallable { f.call() }
 
 fun Action0.toCompletable(): Completable = Completable.fromAction(this)
+fun <T> Function0<T>.toCompletable(): Completable = Completable.fromCallable { this.invoke() }
+
+fun <T> KCallable<T>.toCompletable(): Completable = Completable.fromCallable { this.call() }
+
 fun <T> Callable<T>.toCompletable(): Completable = Completable.fromCallable { this.call() }
 fun <T> Future<T>.toCompletable(): Completable = Completable.fromFuture(this)
 fun <T> Single<T>.toCompletable(): Completable = Completable.fromSingle(this)
