a2db9b7dd408705af3426e3deab1b15f21aece9f
debop <sunghyouk.bae@gmail.com>
2016-06-24 13:15:36 +0900

    -. eclipse-collection 관련 Extension Function 구현

---
+++ debop4k-core/src/kotlin/debop4k/core/utils/eclipseCollections.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2010-2015 sunghyouk.bae@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:JvmName("eclipsecollections")
+
+package debop4k.core.utils
+
+import org.eclipse.collections.impl.list.mutable.FastList
+import org.eclipse.collections.impl.list.mutable.primitive.*
+import org.eclipse.collections.impl.map.mutable.UnifiedMap
+import org.eclipse.collections.impl.set.mutable.UnifiedSet
+
+//
+// Eclipse Collections
+//
+
+// Primitive array
+
+fun ByteArray.toByteArrayList(): ByteArrayList = ByteArrayList.newListWith(*this)
+fun byteArrayListOf(vararg values: Byte): ByteArrayList = ByteArrayList.newListWith(*values)
+
+fun CharArray.toCharArrayList(): CharArrayList = CharArrayList.newListWith(*this)
+fun charArrayListOf(vararg values: Char): CharArrayList = CharArrayList.newListWith(*values)
+
+fun ShortArray.toShortArrayList(): ShortArrayList = ShortArrayList.newListWith(*this)
+fun shortArrayListOf(vararg values: Short): ShortArrayList = ShortArrayList.newListWith(*values)
+
+fun IntArray.toIntArrayList(): IntArrayList = IntArrayList.newListWith(*this)
+fun intArrayListOf(vararg values: Int): IntArrayList = IntArrayList.newListWith(*values)
+
+fun LongArray.toLongArrayList(): LongArrayList = LongArrayList.newListWith(*this)
+fun longArrayListOf(vararg values: Long): LongArrayList = LongArrayList.newListWith(*values)
+
+fun FloatArray.toFloatArrayList(): FloatArrayList = FloatArrayList.newListWith(*this)
+fun floatArrayListOf(vararg values: Float): FloatArrayList = FloatArrayList.newListWith(*values)
+
+fun DoubleArray.toDoubleArrayList(): DoubleArrayList = DoubleArrayList.newListWith(*this)
+fun doubleArrayListOf(vararg values: Double): DoubleArrayList = DoubleArrayList.newListWith(*values)
+
+// FastList
+//
+fun <T> Array<out T>.toFastList(): FastList<T> = FastList.newListWith(*this)
+
+fun <T> emptyFastList() = FastList.newList<T>()
+fun <T> fastListOf(): FastList<T> = emptyFastList()
+fun <T> fastListOf(vararg elements: T): FastList<T> {
+  return if (elements.size == 0) FastList.newList() else elements.toFastList()
+}
+
+// Unified Set
+fun <T> Array<out T>.toUnifiedSet(): UnifiedSet<T> = UnifiedSet.newSetWith(*this)
+
+fun <T> emptyUnifiedSet(): UnifiedSet<T> = UnifiedSet.newSet()
+fun <T> unifiedSetOf(): UnifiedSet<T> = emptyUnifiedSet()
+fun <T> unifiedSetOf(vararg values: T): UnifiedSet<T> = UnifiedSet.newSetWith(*values)
+
+// Unified Map
+//
+fun <K, V> Map<K, V>.toUnifiedMap(): UnifiedMap<K, V> {
+  val map = UnifiedMap.newMap<K, V>()
+  if (this.size > 0) {
+    for ((k, v) in this) {
+      map.put(k, v)
+    }
+  }
+  return map
+}
+
+fun <K, V> unifiedMapOf(vararg pairs: Pair<K, V>): UnifiedMap<K, V> {
+  val map = UnifiedMap.newMap<K, V>(pairs.size)
+  for ((k, v) in pairs) {
+    map.put(k, v)
+  }
+  return map
+}
--- debop4k-core/src/kotlin/debop4k/core/utils/objects.kt
+++ debop4k-core/src/kotlin/debop4k/core/utils/objects.kt
@@ -1,13 +1,26 @@
 /*
- * Copyright (c) 2016. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ * Copyright 2010-2015 sunghyouk.bae@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 @file:JvmName("objects")
 
 package debop4k.core.utils
 
-
+/**
+ * kotlin 수형 중 nullable 이 아니게 선언된 변수의 초기화를 수행할 때 사용합니다.
+ * <code>
+ *   var person:Person = uninitialized()
+ * </code>
+ */
 fun <T> uninitialized() = null as T
