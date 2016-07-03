e12560a31b9bc44c9513a0467bb1ffc41d783f27
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-07-03 22:57:50 +0900

    -. function compose 재정의

--- debop4k-core-java6/src/main/kotlin/debop4k/core/functional/compositions.kt
+++ debop4k-core-java6/src/main/kotlin/debop4k/core/functional/compositions.kt
@@ -1,11 +1,10 @@
 /*
- * Copyright 2016 Sunghyouk Bae<sunghyouk.bae@gmail.com>
- *
+ * Copyright (c) 2016. Sunghyouk Bae <sunghyouk.bae@gmail.com>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,12 +17,14 @@
 
 package debop4k.core.functional
 
-infix fun<T, K, R> Function1<T, K>.andThen(f: (K) -> R): (T) -> R = forwardCompose(f)
+inline infix fun <A, B, C> Function1<A, B>.forwardCompose(crossinline f: (B) -> C): (A) -> C
+    = { a -> f(this(a)) }
 
-infix fun<T, K, R> Function1<T, K>.forwardCompose(f: (K) -> R): (T) -> R {
-  return { t: T -> f(this(t)) }
-}
+infix fun <A, B, C> Function1<A, B>.andThen(f: (B) -> C): (A) -> C = forwardCompose(f)
 
-infix fun<K, R, T> Function1<K, R>.compose(f: (T) -> K): (T) -> R {
-  return { t: T -> this(f(t)) }
-}
+/**
+ * 두 함수를 결합합니다.
+ *  (::length).compose(::isOdd)
+ */
+inline infix fun <A, B, C> Function1<B, C>.compose(crossinline f: (A) -> B): (A) -> C
+    = { a: A -> this(f(a)) }
