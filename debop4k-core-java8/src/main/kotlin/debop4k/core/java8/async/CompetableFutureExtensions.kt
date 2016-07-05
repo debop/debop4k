ccaa185b2f46d3b53692616095b3eea2d026a64c
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-07-05 21:57:03 +0900

    -. @JvmOverloads 추가

--- debop4k-core-java8/src/main/kotlin/debop4k/core/java8/async/CompetableFutureExtensions.kt
+++ debop4k-core-java8/src/main/kotlin/debop4k/core/java8/async/CompetableFutureExtensions.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016. Sunghyouk Bae <sunghyouk.bae@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:JvmName("CompletableFutureExtensions")
 
 package debop4k.core.java8.async
@@ -5,15 +20,18 @@ package debop4k.core.java8.async
 import java.util.concurrent.*
 import java.util.function.*
 
-fun runAsync(action: () -> Unit, executor: Executor = ForkJoinPool.commonPool()): CompletableFuture<Void>
-    = CompletableFuture.runAsync(Runnable(action), executor)
+@JvmOverloads
+inline fun runAsync(crossinline action: () -> Unit,
+                    executor: Executor = ForkJoinPool.commonPool()): CompletableFuture<Void>
+    = CompletableFuture.runAsync(Runnable { action() }, executor)
 
-fun <T> supplyAsync(supplier: () -> T, executor: Executor = ForkJoinPool.commonPool()): CompletableFuture<T>
+@JvmOverloads
+fun <T> supplyAsync(supplier: () -> T,
+                    executor: Executor = ForkJoinPool.commonPool()): CompletableFuture<T>
     = CompletableFuture.supplyAsync(Supplier(supplier), executor)
 
-fun <T> completedFuture(result: T): CompletableFuture<T>
-    = CompletableFuture.completedFuture(result)
-
+fun <T> T.completedFuture(): CompletableFuture<T>
+    = CompletableFuture.completedFuture(this)
 
 
 
