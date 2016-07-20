f92eb9a5f5fe6d53f53faa56cbef702b53148e49
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-07-20 21:27:49 +0900

    -. refactoring

--- debop4k-core-java6/src/test/kotlin/debop4k/core/SystemsTest.kt
+++ debop4k-core-java6/src/test/kotlin/debop4k/core/SystemsTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2016. Sunghyouk Bae <sunghyouk.bae@gmail.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package debop4k.core
 
 import org.junit.Test
@@ -8,14 +23,11 @@ import org.junit.Test
 class SystemsTest : AbstractCoreTest() {
 
   @Test fun retrieveJavaSpecificationVersion() {
-    with(SystemEx) {
-      log.debug("java version={}, implementation={}", version, implementationVersion)
-      log.debug("java vendor={}, implementation={}", vendor, implementationVendor)
+    log.debug("java version={}, implementation={}", JAVA_VERSION, JAVA_IMPLEMENTATION_VERSION)
+    log.debug("java vendor={}, implementation={}", JAVA_VENDOR, JAVA_IMPLEMENTATION_VENDOR)
 
       log.debug("Java6={}", isJava6)
       log.debug("Java7={}", isJava7)
       log.debug("Java8={}", isJava8)
-
-    }
   }
 }
