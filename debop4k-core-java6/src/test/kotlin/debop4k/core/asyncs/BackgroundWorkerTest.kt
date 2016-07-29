1bec38fd4e980bf6720df0abcde882b9c3add508
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-07-30 00:50:07 +0900

    -. spring boot banner 추가

--- debop4k-core-java6/src/test/kotlin/debop4k/core/asyncs/BackgroundWorkerTest.kt
+++ debop4k-core-java6/src/test/kotlin/debop4k/core/asyncs/BackgroundWorkerTest.kt
@@ -20,7 +20,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.slf4j.LoggerFactory
-import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import javax.inject.Inject
 
@@ -29,7 +29,7 @@ import javax.inject.Inject
  * @author debop sunghyouk.bae@gmail.com
  */
 @RunWith(SpringJUnit4ClassRunner::class)
-@SpringApplicationConfiguration(classes = arrayOf(BackgroundWorkerConfiguration::class))
+@SpringBootTest(classes = arrayOf(BackgroundWorkerConfiguration::class))
 class BackgroundWorkerTest {
 
   private val log = LoggerFactory.getLogger(javaClass)
