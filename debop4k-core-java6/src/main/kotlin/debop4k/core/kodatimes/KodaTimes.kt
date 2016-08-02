4e3c90093e5f3d042244db3f0e5ba83061617afa
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-08-02 19:15:33 +0900

    -. 버그 수정

--- debop4k-core-java6/src/main/kotlin/debop4k/core/kodatimes/KodaTimes.kt
+++ debop4k-core-java6/src/main/kotlin/debop4k/core/kodatimes/KodaTimes.kt
@@ -1,25 +1,22 @@
 /*
  * Copyright (c) 2016. Sunghyouk Bae <sunghyouk.bae@gmail.com>
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 @file:JvmName("KodaTimes")
 
 package debop4k.core.kodatimes
 
-import debop4k.timeperiod.utils.TimeZones
 import org.eclipse.collections.api.set.ImmutableSet
 import org.joda.time.*
 import org.joda.time.base.AbstractInstant
