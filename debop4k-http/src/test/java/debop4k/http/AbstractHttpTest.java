99e8349925d24526804fd1c96de43bb33ea77d85
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2016-10-02 13:03:12 +0900

    -. copyright 생성이 잘못되었음

--- debop4k-http/src/test/java/debop4k/http/AbstractHttpTest.java
+++ debop4k-http/src/test/java/debop4k/http/AbstractHttpTest.java
@@ -12,7 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */age debop4k.http;
+ */
+
+package debop4k.http;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
